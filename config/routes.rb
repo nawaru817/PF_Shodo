2af0c0da2c8ae087d9271bb1424a3540c3380a7f
nawaru817 <t010mbharada@gmail.com>
2021-04-30 12:43:31 +0000

    [Add] 新規登録、ログイン、トップ画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  scope module: :public do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
